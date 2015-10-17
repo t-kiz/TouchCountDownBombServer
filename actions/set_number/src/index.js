6dd7ee6f21cd044c877586a7bdda362dcea0b15c
izumin5210 <masayuki@izumin.info>
2015-10-18 00:09:00 +0900

    Publish terminal/client id and number via mqtt client

--- actions/set_number/src/index.js
+++ actions/set_number/src/index.js
@@ -1,28 +1,41 @@
 import AWS  from "aws-sdk";
+import mqtt from "mqtt";
 
-import {REGION, TABLE_NAME} from "./constants";
+import {REGION, TABLE_NAME, MQTT_HOST, MQTT_PORT, MQTT_USER, MQTT_PASS} from "./constants";
 
-const dynamoDB = new AWS.DynamoDB({ region: REGION });
+const dynamoDB    = new AWS.DynamoDB({ region: REGION });
+const mqttClient  = mqtt.connect({ host: MQTT_HOST, port: MQTT_PORT, username: MQTT_USER, password: MQTT_PASS });
 
 export default function(event, context) {
   const requestParams = event.requestParameters;
   dynamoDB.putItem(
     {
       TableName: TABLE_NAME,
-      id: {
-        S: `${requestParams.terminalId}:${requestParams.clientId}
-      },
-      name: {
-        S: `${requestParams.number}`
-      }
+      id: { S: getKeyFromRequestParams(requestParams) },
+      name: { S: getValueFromRequestParams(requestParams) }
     },
     (error, data) => {
       if (error) {
         context.fail(error);
       } else {
-        // publish via mqtt
+        mqttClient.on("error", (error) => context.fail(error));
+        mqttClient.publish(
+          getTopicFromRequestParams(requestParams), requestParams.number, {},
+          (packget) => context.succeed(null)
+        );
       }
     }
   );
 }
 
+function getKeyFromRequestParams(requestParams) {
+  return `${requestParams.terminalId}:${requestParams.clientId}`;
+}
+
+function getValueFromRequestParams(requestParams) {
+  return `${requestParams.number}`;
+}
+
+function getTopicFromRequestParams(requestParams) {
+  return `${requestParams.terminalId}:${requestParams.clientId}`;
+}
